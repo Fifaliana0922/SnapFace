7686e95c62b6ea0171b05dbef1de4f73f5c429b2
Fifaliana <tsiferantsoafifaliana@gmail.com>
2026-08-25 17:36:13 +0300

    ajout des routages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { FaceSnapList } from './face-snap-list/face-snap-list';
+import { LandingPageComponent } from './landing-page-component/landing-page-component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'faceSnaps', component: FaceSnapList },
+    { path: '', component: LandingPageComponent },
+];
---
+++ src/app/landing-page-component/landing-page-component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+
+@Component({
+    selector: 'app-landing-page-component',
+    imports: [RouterLink],
+    templateUrl: './landing-page-component.html',
+    styleUrl: './landing-page-component.scss',
+})
+export class LandingPageComponent {
+    constructor(private router: Router) {}
+
+    navigateToFaceSnaps(): void {
+        this.router.navigateByUrl('faceSnaps');
+    }
+}
