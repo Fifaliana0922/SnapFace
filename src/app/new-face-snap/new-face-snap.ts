54dbc41fb0d3a12b0297978a558aac8c4d0495b7
Fifaliana <tsiferantsoafifaliana@gmail.com>
2026-09-03 12:59:16 +0300

    add features reactive forms

--- src/app/new-face-snap/new-face-snap.ts
+++ src/app/new-face-snap/new-face-snap.ts
@@ -1,9 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { map, Observable } from 'rxjs';
+import { FaceSnape } from '../models/face-snap';
+import { AsyncPipe, DatePipe, UpperCasePipe } from '@angular/common';
 
 @Component({
     selector: 'app-new-face-snap',
-    imports: [],
+    imports: [
+        ReactiveFormsModule,
+        AsyncPipe,
+        DatePipe,
+        UpperCasePipe
+    ],
     templateUrl: './new-face-snap.html',
     styleUrl: './new-face-snap.scss',
 })
-export class NewFaceSnap {}
+export class NewFaceSnap implements OnInit {
+    snapForm !: FormGroup;
+    faceSnapPreview$ !: Observable<FaceSnape>;
+
+    constructor(private formBuilder: FormBuilder) { }
+
+    ngOnInit(): void {
+        this.snapForm = this.formBuilder.group({
+            title: [null],
+            description: [null],
+            imageUrl: [null],
+            location: [null],
+        });
+
+        this.faceSnapPreview$ = this.snapForm.valueChanges.pipe(
+            map(formValue => ({
+                ...formValue,
+                createAt: new Date(),
+                id: 0,
+                snaps: 0
+            }))
+        );
+    }
+
+    onSubmitForm(): void {
+        console.log(this.snapForm.value);
+    }
+}
